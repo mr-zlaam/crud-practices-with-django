39b2f68ee1df2f4d74c53b27f5863c178988e5d2
Zlaam <mrzalaam@gmail.com>
2023-12-12 19:00:38 +0500

    first backend commit

---
+++ Backend/cor/todo_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Todo_App(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (self.title, self.description)
